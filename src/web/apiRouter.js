3ca1d6127ba8f1fb25c92757a5a7eba93018c5d2
Hrvoje Vucic <hrvoje.vucic@hotmail.com>
2018-05-26 16:45:02 +0200

    Refine rotuer

--- src/web/apiRouter.js
+++ src/web/apiRouter.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 
+const { router: authRouter } = require('../auth');
 const { router: userRouter } = require('../users');
 
 const router = express.Router();
 
+router.use('/auth', authRouter);
 router.use('/users', userRouter);
 
 module.exports = { router };
--- src/web/router.js
+++ src/web/router.js
@@ -3,12 +3,10 @@
 const express = require('express');
 
 const { router: apiRouter } = require('./apiRouter');
-const { router: authRouter } = require('../auth');
 
 function register(app, { basePath, apiPath }) {
   const router = express.Router();
 
-  router.use('/auth', authRouter);
   router.use(apiPath, apiRouter);
 
   app.use(basePath, router);
