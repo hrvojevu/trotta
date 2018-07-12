e6f4ca336f981a75e40580fa9bda7eddf4ec6d3d
Hrvoje Vucic <hrvoje.vucic@hotmail.com>
2018-07-12 21:25:21 +0200

    Add material design icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
+      { rel: 'stylesheet', href: 'https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css' },
     ]
   },
 
