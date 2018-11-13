2651a494eeeaee1f01cb3af83f2078040916235e
Hrvoje Vucic <hrvoje.vucic@hotmail.com>
2018-11-13 12:17:29 +0100

    Extend pool-view to be able to edit pools

--- webapp/pool/pool-view.vue
+++ webapp/pool/pool-view.vue
@@ -1,9 +1,17 @@
 <template>
-  <v-layout>
+  <v-layout row wrap>
     <v-flex xs2>
       <v-card>
         <v-card-title primary-title>
           <h3 class="headline mb-0">{{ pool.name }}</h3>
+          <v-btn
+            flat
+            icon
+            color="primary"
+            @click.stop="isPoolDialogShown = true"
+          >
+            <v-icon>settings</v-icon>
+          </v-btn>
         </v-card-title>
         <v-card-text>
           <p>Dužina: {{ pool.length || '-' }} m</p>
@@ -12,13 +20,29 @@
         </v-card-text>
       </v-card>
     </v-flex>
+    <pool-create-edit-dialog
+      v-if="isPoolDialogShown"
+      :dialog="isPoolDialogShown"
+      :pool-prop="pool"
+      @close="isPoolDialogShown = false"
+    />
   </v-layout>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 
+import PoolCreateEditDialog from './pool-create-edit-dialog';
+
 export default {
+  components: {
+    PoolCreateEditDialog,
+  },
+  data() {
+    return {
+      isPoolDialogShown: false,
+    };
+  },
   computed: {
     ...mapGetters('pool', ['getById']),
     pool() {
